c91f54ca6eda386ab28061badbd8b1f28586d5db
Alexandr Ilyushechkin <progruzovik@gmail.com>
2024-06-17 20:58:49 +0300

    add indexRouter

--- src/main/kotlin/ru/mosmetro/backend/config/WebConfig.kt
+++ src/main/kotlin/ru/mosmetro/backend/config/WebConfig.kt
@@ -1,8 +1,16 @@
 package ru.mosmetro.backend.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.Resource
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.HandlerMapping
+import org.springframework.web.reactive.function.server.RequestPredicates.GET
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
 import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.session.WebSessionManager
@@ -11,6 +19,19 @@ import ru.mosmetro.backend.websocket.MetroWebsocketHandler
 
 @Configuration
 class WebConfig {
+
+    @Bean
+    fun indexRouter(@Value("classpath:/public/index.html") html: Resource): RouterFunction<ServerResponse> {
+        return RouterFunctions.route(
+            GET("/{path1:(?!api|v3|\\.).[^.]*}")
+                .or(GET("/{path1:(?!api|v3|\\.).[^.]*}/{path2:[^.]*}"))
+                .or(GET("/{path1:(?!api|v3|\\.).[^.]*}/{path2:[^.]*}/{path3:[^.]*}"))
+        ) { _: ServerRequest? ->
+            ServerResponse.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .bodyValue(html)
+        }
+    }
 
     @Bean
     fun handlerMapping(websocketHandler: MetroWebsocketHandler): HandlerMapping {
